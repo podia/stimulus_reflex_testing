c3924c606c5cfcf0d203a4d291cd2bd721ee9934
Jason Charnes <jason@thecharnes.com>
2020-06-10 17:29:37 -0500

    Stub session in RSpec

--- lib/stimulus_reflex/test_reflex_patches.rb
+++ lib/stimulus_reflex/test_reflex_patches.rb
@@ -1,16 +1,4 @@
 module StimulusReflex::TestReflexPatches
-  class ActionDispatch::Request
-    def session
-      StimulusReflex::Reflex::Session.new
-    end
-  end
-
-  class Session
-    def load!
-      true
-    end
-  end
-
   def get(instance_variable)
     instance_variable_get("@#{instance_variable}")
   end
--- lib/stimulus_reflex_testing/rspec.rb
+++ lib/stimulus_reflex_testing/rspec.rb
@@ -5,5 +5,11 @@ RSpec.configure do |config|
   if defined?(StimulusReflex)
     config.include Rails.application.routes.url_helpers, type: :reflex
     config.include RSpec::Rails::ReflexExampleGroup, type: :reflex
+
+    config.before type: :reflex do
+      allow_any_instance_of(ActionDispatch::Request).to(
+        receive(:session).and_return(double(:session, load!: true))
+      )
+    end
   end
 end
